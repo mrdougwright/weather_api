8e3d58638b19056572f1e5425fbcbcc3ab7922df
Doug Wright <mrdougwright@gmail.com>
2025-05-17 10:51:45 -0500

    Add redis caching

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,6 +5,14 @@ class WeatherController < ApplicationController
     location = Geocoder.search(address).first
     lat, lon = location.latitude, location.longitude
 
+    cache_key = "weather:#{location.place_id}"
+    cached_forecast = Rails.cache.read(cache_key)
+
+    if cached_forecast
+      render json: cached_forecast.merge(from_cache: true)
+      return
+    end
+
     # Call Open Weather API
     # https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
     response = HTTP.get("https://api.openweathermap.org/data/2.5/weather", params: {
@@ -22,6 +30,8 @@ class WeatherController < ApplicationController
     data = JSON.parse(response.body.to_s)
     forecast = ForecastParser.parse(data)
 
-    render json: forecast
+    Rails.cache.write(cache_key, forecast, expires_in: 30.minutes)
+
+    render json: forecast.merge(from_cache: false)
   end
 end
